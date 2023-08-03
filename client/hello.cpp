f86eae9d5a9317cacb7bb986915181403b549ddb
jitong_zeng <jitong_zeng@sunwave.com.cn>
2023-08-03 17:18:32 +0800

    add hello.cpp

---
+++ client/hello.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file hello.cpp
+ * @author jitong_zeng (jitong_zeng@sunwave.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-03  17:17:48
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+int print_hello(const char *) {
+
+    return 0;
+}
